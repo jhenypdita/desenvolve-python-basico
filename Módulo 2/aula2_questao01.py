396a1d13a38e3efbdc140c9dca02e8a5ac886616
Jheniffe Santos Andrade <153311825+jhenypdita@users.noreply.github.com>
2025-06-30 15:56:19 -0300

    Add files via upload
    Atividades aula 2

---
+++ Módulo 2/aula2_questao01.py
@@ -0,0 +1,19 @@
+#1 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+#a. Armazene o valor 5 em uma variável e o valor 2 em outra variável
+v1 = 5
+v2 = 2
+
+#b. Imprima o tipo de dados dessas duas variáveis
+print (type(v1))
+print (type(v2))
+
+#c. Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+v3 = v1 / v2
+
+#d. Imprima a terceira variável e também o seu tipo
+print(v3)
+print(type(v3))
+
+
+ 
---
+++ Módulo 2/aula2_questao02.py
@@ -0,0 +1,26 @@
+#2 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+#1. Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+
+texto = "O resuldado é"
+x = 10
+y = 3.5
+
+#2. Some os valores da segunda e terceira variável e armazene em outra variável.
+
+soma = x + y
+
+#3. Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+print (texto)
+print (type(texto))
+
+
+print (x)
+print (type(x))
+
+print (y)
+print (type(y))
+
+print (soma)
+print (type(soma))
